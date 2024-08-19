3e01e5a893f5bb096346e9ab3a89e6c6ace1b9dc
Leon Chou <iamyiii0328@gmail.com>
2024-08-20 02:15:37 +0800

    style: 調整程式碼順序, 刪除分號

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createAppRouter } from '@/router/index';
+import { createAppRouter } from '@/router/index'
 import App from './App.vue'
 
-import i18n from '@/plugins/i18n';
-import versionPlugin from '@/plugins/version';
+import i18n from '@/plugins/i18n'
+import versionPlugin from '@/plugins/version'
 
-import '@/assets/normalize.css';
+import '@/assets/normalize.css'
 
-const { router } = createAppRouter();
-const pinia = createPinia();
+const { router } = createAppRouter()
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
 app.use(i18n)
-app.use(versionPlugin);
+app.use(versionPlugin)
 
 app.mount('#app')
