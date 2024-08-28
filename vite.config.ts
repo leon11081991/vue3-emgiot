aca00701a48ef850b70e5fbad3e1d5d49ee16b48
Leon Chou <leonchou@funday.asia>
2024-08-28 18:25:50 +0800

    feat(svg-icon): vite-svg-icon設定,建立組件
    1. 引入相關檔案
    2. 設定config
    3. 建立組件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,6 +21,11 @@ export default defineConfig(({ mode }) => {
     plugins: [
       vue(),
       vueDevTools(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+        svgoOptions: true
+      })
     ],
     resolve: {
       alias: {
@@ -27,8 +35,7 @@ export default defineConfig(({ mode }) => {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import "./src/assets/variables.scss";
-          @import "./src/assets/scss/mixin.scss";`
+          additionalData: `@import "./src/assets/style.scss";`
         }
       }
     }
