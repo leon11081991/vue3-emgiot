c1a2e19bfe804a87c8c4dd2298f9f6b92eb9cb26
Leon Chou <leonchou@funday.asia>
2024-08-29 18:36:09 +0800

    feat(dashboard-page): 建立更新動態header title 範例

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+import { useHeader } from '@/composables/useHeader'
+
+import { useI18n } from 'vue-i18n'
+
+const { t: $t } = useI18n()
+const { updateHeaderTitle } = useHeader()
+
+const storeName = ref('')
+
+onMounted(() => {
+  storeName.value = '大寮光華店'
+
+  updateHeaderTitle($t('DashboardPage.HeaderTitle') + storeName.value) // 設定動態 header title
+})
+</script>
 
 <template>
   <div class="dashboard-page">
