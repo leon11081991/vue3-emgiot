b87b1b75fc7220f49cce82e15a5c86e31daaf950
Leon Chou <leonchou@funday.asia>
2024-08-29 18:32:32 +0800

    feat(i18n): 增加header多國語系資料

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -3,6 +3,7 @@ import tw from '@/lang/tw.json';
 import en from '@/lang/en.json';
 
 const i18n = createI18n({
+  legacy: false,  // 設置為false, 啟用composition API模式
   locale: 'tw',
   fallbackLocale: 'tw',
   globalInjection: true,
