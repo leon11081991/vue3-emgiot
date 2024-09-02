a3bcf72c5cdfb031b90d9fb690fb3eae74bbb0a8
Leon Chou <iamyiii0328@gmail.com>
2024-09-03 01:47:37 +0800

    chore(env): 新增api版本

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,11 @@
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
+const apiVersion = import.meta.env.VITE_API_VERSION || 'v1'
 
 if (!apiBaseUrl) {
   throw new Error('Missing VITE_API_BASE_URL')
 }
 
 export const env = {
-  apiBaseUrl
+  apiBaseUrl,
+  apiVersion
 }
