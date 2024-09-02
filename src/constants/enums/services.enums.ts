f728563ebbd542e6e9e8e2e2dab1fe0d2ef22580
Leon Chou <iamyiii0328@gmail.com>
2024-09-03 01:56:44 +0800

    chore(enums): 新增enums資料

---
+++ src/constants/enums/services.enums.ts
@@ -0,0 +1,13 @@
+export enum SignInEnum {
+  signIn = 'SignIn/SignIn',
+  validate = 'SignIn/Validate',
+  forgetPassword = 'SignIn/ForgetPassword',
+  passwordChange = 'SignIn/PasswordChange',
+  accountDisable = 'SignIn/AccountDisable',
+}
+
+export enum LoginEnum {
+  login = 'LogIn/LogIn',
+  googleLogIn = 'LogIn/GoogleLogIn',
+  logOut = 'LogIn/LogOut'
+}
