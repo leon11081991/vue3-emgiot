e53e2db0bae04d7f741e63eb6a3498ec46b60aaf
Leon Chou <iamyiii0328@gmail.com>
2024-09-03 23:26:46 +0800

    chore(env): 更新環境變數的api路徑

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
-const apiVersion = import.meta.env.VITE_API_VERSION || 'v1'
+const apiVersion = import.meta.env.VITE_API_VERSION
 
 if (!apiBaseUrl) {
   throw new Error('Missing VITE_API_BASE_URL')
