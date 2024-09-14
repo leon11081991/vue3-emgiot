d94f29e00e2b5a52cf65e8cd5ef98a3f646ae8b9
leo <bjmqfg83@gmail.com>
2024-09-14 14:27:22 +0800

    feat: update chart demo page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import type { RouteLocationNormalized } from 'vue-router';
+import type { RouteLocationNormalized } from 'vue-router'
 import { createRouter, createWebHistory } from 'vue-router'
 
 import { layoutMiddleware } from '@/router/middlewares/layoutMiddleware'
@@ -77,6 +77,15 @@ const routes = [
       layout: 'LayoutDefault',
       middleware: [layoutMiddleware]
     }
+  },
+  {
+    path: '/chartTemp',
+    name: 'ChartTemp',
+    component: () => import('@/views/ChartTemp.vue'),
+    meta: {
+      layout: 'LayoutDefault',
+      middleware: [layoutMiddleware]
+    }
   }
 ]
 
@@ -86,15 +95,13 @@ const scrollBehavior = (
   savedPosition: any
 ) => {
   if (savedPosition) {
-    return savedPosition;
+    return savedPosition
   } else {
-    return { top: 0 };
+    return { top: 0 }
   }
-};
-
+}
 
 export const createAppRouter = () => {
-
   const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
@@ -103,35 +110,32 @@ export const createAppRouter = () => {
 
   // 設置middleware
   router.beforeEach((to, from, next) => {
-
     if (!to.meta.middleware) {
       // 沒有設定middleware，允許路由繼續
       return next()
     }
 
-    const middleware = Array.isArray(to.meta.middleware)
-      ? to.meta.middleware
-      : [to.meta.middleware];
-    console.log("middleware", middleware);
-    const context = { to, from, next };
-    let idx = 0; // 初始化中間件索引
+    const middleware = Array.isArray(to.meta.middleware) ? to.meta.middleware : [to.meta.middleware]
+    console.log('middleware', middleware)
+    const context = { to, from, next }
+    let idx = 0 // 初始化中間件索引
 
     // 定義執行中間件的函數
     const run = () => {
       if (idx < middleware.length) {
-        const mw = middleware[idx]; // 獲取當前中間件
+        const mw = middleware[idx] // 獲取當前中間件
         mw({
           ...context,
           next: () => {
-            idx++; // 移動到下一個中間件
-            run(); // 繼續執行下一個中間件
+            idx++ // 移動到下一個中間件
+            run() // 繼續執行下一個中間件
           }
-        });
+        })
       } else {
-        next(); // 所有中間件執行完畢，繼續路由導航
+        next() // 所有中間件執行完畢，繼續路由導航
       }
-    };
-    run(); // 開始執行中間件
+    }
+    run() // 開始執行中間件
   })
 
   return { router }
---
+++ src/views/ChartTemp.vue
@@ -0,0 +1,61 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Bar } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import type { ChartOptions, ChartData } from 'chart.js' // 正確的型別引入方式
+
+// 註冊 Chart.js 的組件
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+// 非響應式變數
+const colorPrimary = '#21cc9e'
+const colorWarning = '#FC8902'
+
+// ref 變數
+const chartData = ref<ChartData<'bar'>>({
+  labels: ['January', 'February', 'March'],
+  datasets: [
+    {
+      data: [40, 20, 12],
+      backgroundColor: [colorPrimary, colorPrimary, colorWarning]
+    }
+  ]
+})
+
+const chartOptions = ref<ChartOptions<'bar'>>({
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false // 隱藏圖例（包括數據集標籤）
+    }
+  },
+  scales: {
+    x: {
+      display: false // 隱藏 x 軸上的 labels
+    },
+    y: {
+      beginAtZero: true // 確保 y 軸從 0 開始
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="chartPage">
+    <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chartPage {
+  width: 800px;
+}
+</style>
