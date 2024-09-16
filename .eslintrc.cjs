d29a4c8a02d70777d135ebd189cef511080a59e1
Leon Chou <iamyiii0328@gmail.com>
2024-09-16 22:43:03 +0800

    chore(lint): 更新eslint設定
    更新為開發環境略過規則,正式環境開啟警告

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,14 +22,19 @@ module.exports = {
   plugins: ['prettier'], // 使用 Prettier plugin 檢查程式碼格式
   rules: {
     'prettier/prettier': 'error', // 啟用 Prettier plugin, 強制執行 Prettier 格式化程式碼，違反時錯誤
-    // 'no-console': process.env.NUXT_ENV === 'prod' ? 'warn' : 'off', // 檢查 console.log，如果是production環境則違反時警告，否則違反時禁止
-    // 'no-undef': 'error', // 檢查未定義的變數，違反時錯誤
-    // 'no-unused-vars': 'warn' // 檢查沒有使用到的變數，違反時警告
+    'no-console': process.env.NUXT_ENV === 'prod' ? 'warn' : 'off', // 檢查 console.log，如果是production環境則違反時警告，否則違反時禁止
+    'no-undef': process.env.NUXT_ENV === 'prod' ? 'error' : 'off', // 檢查未定義的變數，違反時錯誤
+    'no-unused-vars': process.env.NUXT_ENV === 'prod' ? 'warn' : 'off' // 檢查沒有使用到的變數，違反時警告
   },
   overrides: [
     // 其他特定配置
     {
-      files: ['src/views/**/*.{js,ts,vue}', 'src/layouts/**/*.{js,ts,vue}', 'app.{js,ts,vue}' ],
+      files: [
+        'src/views/**/*.{js,ts,vue}',
+        'src/components/**/*.{js,ts,vue}',
+        'src/layouts/**/*.{js,ts,vue}',
+        'app.{js,ts,vue}'
+      ],
       rules: {
         'vue/multi-word-component-names': 'warn' // 組件名稱必須是多個單詞，違反時警告
       }
