95b58666cf3f05cd2c3f874775fb54e1deee7376
Leon Chou <leonchou@funday.asia>
2024-09-16 18:28:54 +0800

    style(lint): 格式化專案

--- src/services/dashboard.services.ts
+++ src/services/dashboard.services.ts
@@ -1,5 +1,9 @@
 import { type ApiResponse, ApiFactory } from '@/utils/api'
-import type { GetOperationChartReqType, GetClawOperationsInfoReqType, GetCoinOperationsInfoReqType } from '@/models/types/dashboard.types'
+import type {
+  GetOperationChartReqType,
+  GetClawOperationsInfoReqType,
+  GetCoinOperationsInfoReqType
+} from '@/models/types/dashboard.types'
 import { DashboardEnum } from '@/constants/enums/api/dashboard.enums'
 
 export class DashboardServices extends ApiFactory {
@@ -9,12 +13,18 @@ export class DashboardServices extends ApiFactory {
 
   /** 取得首頁運營圖表 */
   getOperationChart = async (params: GetOperationChartReqType): Promise<ApiResponse> => {
-    return await this.get<GetOperationChartReqType, ApiResponse>(DashboardEnum.operationsChart, params)
+    return await this.get<GetOperationChartReqType, ApiResponse>(
+      DashboardEnum.operationsChart,
+      params
+    )
   }
 
   /** 取得選物機運營清單 */
   getClawOperationsInfo = async (params: GetClawOperationsInfoReqType): Promise<ApiResponse> => {
-    return await this.get<GetClawOperationsInfoReqType, ApiResponse>(DashboardEnum.clawOperationsInfo, params)
+    return await this.get<GetClawOperationsInfoReqType, ApiResponse>(
+      DashboardEnum.clawOperationsInfo,
+      params
+    )
   }
 
   /** 取得兌幣機運營清單 */
