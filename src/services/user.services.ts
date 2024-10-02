95c84780aa99a0961a90bde6435fbbc15ac213e1
Leon Chou <iamyiii0328@gmail.com>
2024-10-03 03:56:56 +0800

    feat(profile-page): update change user nick and change password
    update:
    1. change password api
    2. change word signin to signup

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,4 +1,5 @@
 import { type ApiResponse, ApiFactory } from '@/utils/api'
+import { SignUpEnum } from '@/constants/enums/api/auth.enums'
 import { UserEnum } from '@/constants/enums/api/user.enums'
 
 export class UserServices extends ApiFactory {
@@ -9,11 +10,6 @@ export class UserServices extends ApiFactory {
   /** 取得使用者資訊 */
   getUserInfo = async () => {
     return await this.get<unknown, ApiResponse>(UserEnum.userInfo)
-  }
-
-  /** 變更密碼 */
-  changePassword = async (newPwd: string) => {
-    return await this.put<string, ApiResponse>(UserEnum.password, newPwd)
   }
 
   /** 變更使用者資訊 */
