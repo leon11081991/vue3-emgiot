85cae89302ff7b9cb4c3c576ce86368cdcb5eb8e
Leon Chou <iamyiii0328@gmail.com>
2024-10-04 00:59:23 +0800

    refactor(header & sidebar): move files to their corresponding directories

---
+++ src/constants/common/header.const.ts
@@ -0,0 +1,27 @@
+// header的項目
+export const headerTitleList = [
+  {
+    name: 'Home',
+    'header-title': '首頁'
+  },
+  {
+    name: 'Profile',
+    'header-title': '個人資料'
+  },
+  {
+    name: 'Member',
+    'header-title': '成員管理'
+  },
+  {
+    name: 'MemberInfo',
+    'header-title': '成員資訊'
+  },
+  {
+    name: 'GroupEdit',
+    'header-title': '編輯自訂分類'
+  },
+  {
+    name: 'Product',
+    'header-title': '商品列表'
+  }
+]
