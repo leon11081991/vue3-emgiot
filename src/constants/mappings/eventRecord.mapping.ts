d1b13338377afa7c42a302fb221199b269cf6094
Leon Chou <iamyiii0328@gmail.com>
2024-11-21 00:56:32 +0800

    feat(issue-73): hide event record option "cashbox"
    comment mapping data in event record mapping
    
    fix #73

--- src/constants/mappings/eventRecord.mapping.ts
+++ src/constants/mappings/eventRecord.mapping.ts
@@ -1,7 +1,8 @@
 export const EVENT_RECORD_MAPPING: Record<string, number[]> = {
   connection: [0, -1], // 0=機台連線 -1=機台斷線
   dispatch: [100], // 100=出幣/出貨
-  cashbox: [110], // 110=錢箱收款
+  /** 目前"錢箱收款"尚未設定實際的功能需求，故先將其隱藏，待日後硬體方面支援此功能後再顯示 */
+  // cashbox: [110], // 110=錢箱收款
   refill: [111], // 111=補幣事件
   dispense: [100], // 100=出幣/出貨
   return: [210], // 210=退幣事件
@@ -13,7 +14,8 @@ export const CLAW_EVENT_TYPE_ICON_MAPPING: Record<number, string> = {
   '-1': 'offline', // 機台斷線
   0: 'online', // 機台連線
   100: 'product', // 電眼出貨
-  110: 'cash-box', // 錢箱收款
+  /** 目前"錢箱收款"尚未設定實際的功能需求，故先將其隱藏，待日後硬體方面支援此功能後再顯示 */
+  // 110: 'cash-box', // 錢箱收款
   111: 'replenish-coins-sm' // 補幣事件
 }
 
