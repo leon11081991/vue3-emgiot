eaff87420557fae57cb5b681e6135d2cab0d80cb
Leon Chou <iamyiii0328@gmail.com>
2024-11-24 01:28:56 +0800

    chore: fix missing files cause by conflict

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -22,10 +24,25 @@ export default defineConfig(({ mode }) => {
       assetsDir: 'assets',
       sourcemap: true
     },
-    plugins: [vue(), vueDevTools()],
+    plugins: [
+      vue(),
+      vueDevTools(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+        svgoOptions: true
+      })
+    ],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/assets/style.scss";`
+        }
       }
     }
   }
